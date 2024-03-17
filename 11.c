7f1a90d8a624fb5013010e8a08d03985ac4ae882
poonamkokitkar <poonamkokitkar19@gmail.com>
2024-03-17 09:10:04 +0530

    solution

---
+++ 11.c
@@ -0,0 +1,17 @@
+/*
+	Q11. Write a program to find factorial of given number.
+*/
+
+#include<stdio.h>
+int main()
+{
+	int num,fact=1;
+	printf("Enter the number : ");
+	scanf("%d",&num);
+
+	for(int i=1; i<= num; i++)
+		fact = fact * i;
+	printf("The factorial of number is : %d\n",fact);
+
+	return 0;
+}
