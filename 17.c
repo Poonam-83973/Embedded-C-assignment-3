82e1ad09d7aec11815f09fd27e5a77f9ff7c8389
poonamkokitkar <poonamkokitkar19@gmail.com>
2024-03-17 14:14:59 +0530

    solution

---
+++ 17.c
@@ -0,0 +1,28 @@
+/*
+	Q17. Print following pattern
+		* * * * *
+		* * * * *
+		* * * * *
+		* * * * *
+		* * * * *
+*/
+
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter the number : ");
+	scanf("%d",&num);
+
+	for(int i=1; i<=num; i++)
+	{
+	  	for(int j=1; j<=num; j++)
+	    {
+	 		printf(" *");
+	    }  
+     	printf("\n");
+	}
+
+	return 0;
+}
+
